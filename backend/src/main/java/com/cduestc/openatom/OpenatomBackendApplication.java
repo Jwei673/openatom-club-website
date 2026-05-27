980c6ac7503b8ef61186e8a363c20db626a3078e
OpenAtom Club <you@example.com>
2026-05-27 22:46:13 +0800

    Backend: Initialize Spring Boot 3 project structure with Maven

---
+++ backend/src/main/java/com/cduestc/openatom/OpenatomBackendApplication.java
@@ -0,0 +1,11 @@
+package com.cduestc.openatom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenatomBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenatomBackendApplication.class, args);
+    }
+}
